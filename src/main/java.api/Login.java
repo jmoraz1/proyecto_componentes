d77edcb023dff9191e408779e8e268245844118b
Jimena <jmoraz1@ucenfotec.ac.cr>
2020-06-22 01:01:36 -0600

    updating web.xml

--- src/main/java.api/Login.java
+++ src/main/java.api/Login.java
@@ -1,11 +1,12 @@
 package java.api;
 
 import javax.ws.rs.*;
+
+
 @Path ("login")
 public class Login {
 	@GET
 	@PRODUCES("application/xml")
-	
 	public String requestLogin() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("<note>");
